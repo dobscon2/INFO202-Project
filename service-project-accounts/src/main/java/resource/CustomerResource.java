4b10bb4077647b2e0cca760e5ab3827a02ebccef
Connor Dobson <dobco402@student.otago.ac.nz>
2021-04-10 16:10:29 +1200

    Forgot to add an id parameter into the Resource URL

--- service-project-accounts/src/main/java/resource/CustomerResource.java
+++ service-project-accounts/src/main/java/resource/CustomerResource.java
@@ -44,6 +44,41 @@ public class CustomerResource extends Jooby {
 				}
 
 			});
+                        
+                        use("/:id", (req, rsp, chain) -> {
+				String id = req.param("id").value();
+
+				if (dao.exists(id)) {
+					// ID is OK, so pass request on to the next route in the chain
+					chain.next(req, rsp);
+				} else {
+					// Bad ID - send a 404 response
+					rsp.status(Status.NOT_FOUND).send(new ErrorMessage("There is no customer matching that ID."));
+				}
+			});
+
+			get("/:id", (req) -> {
+				String id = req.param("id").value();
+				return dao.getCustomerById(id);
+			});
+
+			delete("/:id", (req, rsp) -> {
+				String id = req.param("id").value();
+				dao.deleteCustomer(id);
+				rsp.status(Status.NO_CONTENT);
+			});
+
+			put("/:id", (req, rsp) -> {
+				String id = req.param("id").value();
+				Customer customer = req.body().to(Customer.class);
+
+				if (!id.equals(customer.getId())) {
+					rsp.status(Status.CONFLICT).send(new ErrorMessage("Modifying the customer ID via this operation is not allowed."));
+				} else {
+					dao.updateCustomer(id, customer);
+					rsp.status(Status.NO_CONTENT);
+				}
+			});
 
 		}).produces(MediaType.json).consumes(MediaType.json);
 
